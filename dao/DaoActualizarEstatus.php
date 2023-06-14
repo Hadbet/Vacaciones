6a2c86a4df179d6e16fedd8e96ff63a8b7ef6259
hadbet <hadbet.altamirano@grammer.com>
2023-06-14 15:12:09 -0600

    Se actualizo y agrego el cambio de estatus en la parte del termino del tiempo ciclo

---
+++ dao/DaoActualizarEstatus.php
@@ -0,0 +1,24 @@
+<?php
+include_once('DataBase/Vacaciones.php');
+
+$Token = $_GET("6486f0e4");
+
+actualizarEstatus($Token);
+function actualizarEstatus($Token){
+
+    $con = new LocalConector();
+    $conex=$con->conectar();
+
+    $insertRegistro= "UPDATE `FilaVirtual` SET `Estatus`='0' WHERE `Token` = '$Token'";
+
+    $rsinsertUsu=mysqli_query($conex,$insertRegistro);
+    mysqli_close($conex);
+
+    if(!$rsinsertUsu){
+        echo '{"data":[{"Estatus":"0"}]}';
+    }else{
+        echo '{"data":[{"Estatus":"1"}]}';
+    }
+}
+
+?>
