66b4ab498427ddbafa145a011c831c74f221e82c
hadbet <hadbet.altamirano@grammer.com>
2023-06-14 14:39:07 -0600

    Se asigno la db

--- dao/ConsultaFila.php
+++ dao/ConsultaFila.php
@@ -1,7 +1,7 @@
 <?php
 
 
-include_once('db.php');
+include_once('DataBase/Vacaciones.php');
 
 $TipoEncargado = $_GET['tipoEncargado'];
 
--- dao/DaoConsultaTurno.php
+++ dao/DaoConsultaTurno.php
@@ -1,6 +1,5 @@
 <?php
-include_once('db.php');
-
+include_once('DataBase/Vacaciones.php');
 Contador();
 /*
 $TurnoAux = obtenerValorConsulta();
