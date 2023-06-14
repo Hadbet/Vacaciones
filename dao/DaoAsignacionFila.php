75cb405c21086f143a1b09e346668537c8694cd4
hadbet <hadbet.altamirano@grammer.com>
2023-06-14 12:38:01 -0600

    Correccion de Fila Turno

--- dao/DaoAsignacionFila.php
+++ dao/DaoAsignacionFila.php
@@ -19,8 +19,6 @@ function registroUsu($Token,$Turno,$DateAndTime){
         return 1;
     }
 }
-
-obtenerValorConsultaAux();
 function obtenerValorConsultaAux() {
     $con = new LocalConector();
     $conex = $con->conectar();
