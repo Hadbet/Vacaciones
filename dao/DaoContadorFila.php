e0bd86d709900f085b25f8cd7e8fab0f7ee681c2
hadbet <hadbet.altamirano@grammer.com>
2023-06-14 15:56:40 -0600

    Contador de la fila y minutos

---
+++ dao/DaoContadorFila.php
@@ -0,0 +1,19 @@
+<?php
+
+
+include_once('DataBase/Vacaciones.php');
+
+Contador();
+
+function Contador($TipoEncargado)
+{
+    $con = new LocalConector();
+    $conex = $con->conectar();
+    $datos = mysqli_query($conex, "SELECT COUNT(`IdFila`) as Conteo FROM `FilaVirtual` WHERE `Estatus`='1';");
+
+    $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
+    echo json_encode(array("data" => $resultado));
+}
+
+?>
+
